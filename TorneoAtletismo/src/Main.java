ce5279d741ff27a0b64c62839a6004c389cfbd40
Tunchezzz <sebastiantunchez2007@gmail.com>
2026-08-17 12:32:04 -0600

    Creación de las carpetas necesarias para el Ejercicio #2 de competencia de atletismo.

---
+++ TorneoAtletismo/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] lista;
+        int a=0;
+        lista=new int[2];
+        for (int numero:lista){
+            if (numero!=0){
+                a++;
+            }
+        }
+        System.out.println(a);
+    }
+}
